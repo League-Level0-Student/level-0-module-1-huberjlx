931051cc3453980d407d5e359c353113b2069818
root <root@48fb8092d548>
2020-07-06 23:26:42 +0000

    working on zombie

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,33 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String answer = JOptionPane.showInputDialog(null, "32 white horses on a red hill. They stomp, and stamp, and buck. " + "\n"+
+"Then they stop. What are they?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (answer.equalsIgnoreCase("teeth")) {
+	
+	JOptionPane.showMessageDialog(null, "Correct");
+	score++;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	
+	JOptionPane.showMessageDialog(null, "Wrong, it was teeth");
+}
 		// 6. Add some more riddles
-
+String answer2 = JOptionPane.showInputDialog("It has many nuts and bolts. Shows people things." + "\n" +
+		"It takes any input gives you anything back");
 		// 2. Make a pop up to show the score.
-		
+		if (answer2.equalsIgnoreCase("a Computer")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong, it was a computer");
+		}
+		JOptionPane.showMessageDialog(null, score);
 	}
 }
 
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,12 @@
 
 void setup() {
-
+size(500,500);
 }
 void draw() {
-
+  fill(255,0,0);
+ellipse(150,100, 50, 50);
+ellipse(350,100,50,50) 
+fill(0,0,0);
+ellipse(150,100,25,25);
+ellipse(350,100,25,25);
 }
